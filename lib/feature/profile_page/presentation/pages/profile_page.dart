0dfe78ed35c3fae7193b70170963d6f3ec12e388
PLUXVR <pluxar@bk.ru>
2024-11-17 18:27:24 +0300

    profile page first commit

--- lib/feature/profile_page/presentation/pages/profile_page.dart
+++ lib/feature/profile_page/presentation/pages/profile_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vas_app/core/resources/assets/resources.dart';
+import 'package:vas_app/core/widgets/default_app_bar.dart';
 import 'package:vas_app/feature/app/routing/route_path.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -7,8 +10,45 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Профиль'),),
+    return Scaffold(
+      appBar: DefaultAppBar(
+        titleText: 'Никита Алексеевич',
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: SizedBox(
+                height: 100,
+                width: 100,
+                child: CircleAvatar(
+                  child: SvgPicture.asset(
+                    VectorAssets.icProfileActive,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 25.0,
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Моя информация',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
